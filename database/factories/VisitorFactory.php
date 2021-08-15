6fe070ff18560ba941e2f33464dc394026c1dc91
“jlavadoww” <jonatan.lavado@ww.com>
2021-08-15 12:43:57 -0500

    dia-06 Create factories for the models

---
+++ database/factories/VisitorFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Visitor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VisitorFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Visitor::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'dni' => $this->faker->regexify('[1-9]{8}'),
+            'entity' => $this->faker->company()
+        ];
+    }
+}
